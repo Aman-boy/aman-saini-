8230771fc893048ede7d7bb3b797dfa84861699a
Aman-boy <aman70702610@gmail.com>
2025-10-16 08:55:29 +0530

    Change credit name in vars.py
    Updated credit information in vars.py.

--- vars.py
+++ vars.py
@@ -7,7 +7,8 @@ API_ID = int(environ.get("API_ID", "23157553"))
 API_HASH = environ.get("API_HASH", "5dac7edae7d76509bd3b66a70e6bc320")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 OWNER = int(environ.get("OWNER", "8150081319"))
-CREDIT = environ.get("CREDIT", "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎")
+CREDIT = environ.get("CREDIT", "AMAN 𝘽𝙊𝙏𝙎")
 LOG_CHANNEL = "-1002701630809" # log group me bot ko admin narur banaye full rights ke sath
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
+
